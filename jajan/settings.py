b8e6e5479b36eb81bbc8ee30a05d31591dda5ac1
Fajran Iman Rusadi <fajran@gmail.com>
2020-09-22 21:39:55 +0700

    Remove Django key

--- jajan/settings.py
+++ jajan/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h7tf+xvj-y0fii(1hyjzmb2v=+$v@dbht7!9aqlwjy!knye4j1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
